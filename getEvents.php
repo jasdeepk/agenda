28cdeb26cdda1a5ccee8bd1e8e54c558ca13e997
Nbalsh <brandon.bn.nelson@gmail.com>
2015-01-04 20:15:24 -0800

    Some initial php
    :o must be a better way to query

---
+++ getEvents.php
@@ -0,0 +1,76 @@
+<?php
+	// http://www.w3schools.com/Ajax/ajax_php.asp
+
+	// get requests meeting type, time, date, user
+	$type = $_GET['q']; // from open("GET", "thisFileName.php?q=" + meetingType+"&t_hout"+hourTime, true);
+	$t_hour = $_GET["t_hour"];
+	$t_min = $_GET["t_min"];
+	$user = $_GET["user"];
+	$date = $_GET["date"];
+	// assume hour and minute must be given together
+	
+	
+	/*
+	//$con = mysqli_connect( );
+	if(mysqli_connect_errno($con)){
+		echo "Failed to connect to DataBase: " .mysqli_connect_error();
+	}
+	*/
+	
+	if (empty($t_user)){
+		// error, need a user to lookup
+	}
+	// query if all items
+	if(!empty($t_hour) && !empty($t_min) && !empty($type) && !empty($date)){
+		$result = $con->query("SELECT * FROM Event 
+		WHERE 
+		id = '$user' AND 
+		typeE = $type AND 
+		dateE = $date AND 
+		thour = $t_hour AND
+		tmin = $t_min");
+	}
+	//query if day and type
+	if(empty($t_hour) && empty($t_min) && !empty($type) && !empty($date)){
+		$result = $con->query("SELECT * FROM Event 
+		WHERE 
+		id = '$user' AND 
+		typeE = $type AND 
+		dateE = $date");	
+	}
+	//query if type and time
+	if(!empty($t_hour) && !empty($t_min) && !empty($type) && empty($date)){
+		$result = $con->query("SELECT * FROM Event 
+		WHERE 
+		id = '$user' AND 
+		typeE = $type AND
+		thour = $t_hour AND
+		tmin = $t_min");
+	}
+	//query if day and time
+	if(!empty($t_hour) && !empty($t_min) && empty($type) && !empty($date)){
+		$result = $con->query("SELECT * FROM Event 
+		WHERE 
+		id = '$user' AND  
+		dateE = $date AND 
+		thour = $t_hour AND
+		tmin = $t_min");
+	}
+	//query if day and type
+	if(empty($t_hour) && empty($t_min) && !empty($type) && !empty($date)){
+		$result = $con->query("SELECT * FROM Event 
+		WHERE 
+		id = '$user' AND 
+		typeE = $type AND 
+		dateE = $date");
+	}
+	//query if just day
+	if(empty($t_hour) && empty($t_min) && empty($type) && !empty($date)){
+		$result = $con->query("SELECT * FROM Event 
+		WHERE 
+		id = '$user' AND 
+		dateE = $date");
+	}
+	
+	
+?>
